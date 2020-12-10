094266e0f01a33aab6d2cbf03c789e57229b338b
Alex Rudnick <alex.rudnick@gmail.com>
2020-12-10 12:00:02 -0800

    pull out a function for loading up all the date counts and make main pymodoro loop report current count for the day

--- pymodoro.py
+++ pymodoro.py
@@ -8,6 +8,7 @@ DATETIMEFORMAT = "%Y-%m-%d %H:%M"
 import datetime
 import time
 import os
+from collections import Counter
 
 def save_pomodoro():
     timestamp = datetime.datetime.now().strftime(DATETIMEFORMAT)
@@ -15,6 +16,26 @@ def save_pomodoro():
     worklogfn = os.path.join(home, PYMODORO_DIR, "worklog")
     with open(worklogfn, "a") as outfile:
         print("PYMODORO at", timestamp, file=outfile)
+
+def load_counts():
+    """Returns a Counter mapping day tuples (Y, M, D) to counts of pymodoros."""
+    by_year = Counter()
+    by_month = Counter()
+    by_day = Counter()
+
+    prefix = "PYMODORO at "
+    home = os.path.expanduser("~")
+    worklogfn = os.path.join(home, PYMODORO_DIR, "worklog")
+    with open(worklogfn) as infile:
+        for line in infile:
+            line = line.strip()
+            if line.startswith(prefix):
+                date_string = line[len(prefix):]
+                dt = datetime.datetime.strptime(date_string, DATETIMEFORMAT)
+                by_year[dt.year] += 1
+                by_month[(dt.year, dt.month)] += 1
+                by_day[(dt.year, dt.month, dt.day)] += 1
+    return by_day
 
 def main():
     while True:
@@ -26,6 +47,11 @@ def main():
         print("hit enter to log success and take a break! (ctrl-c to exit)")
         input("pause: ")
         save_pomodoro()
+        by_day = load_counts()
+        now = datetime.datetime.now()
+        today_key = (now.year, now.month, now.day)
+        print("TODAY %d-%02d-%02d\t%d" % (now.year, now.month, now.day,
+                                          by_day[today_key]))
         for minutesleft in range(BREAKLENGTH, 0, -1):
             print("break for {} minutes".format(minutesleft))
             time.sleep(60)
--- summarize.py
+++ summarize.py
@@ -3,33 +3,15 @@
 import datetime
 import os
 import sys
-from collections import Counter
 
-from pymodoro import PYMODORO_DIR 
-from pymodoro import DATETIMEFORMAT
+import pymodoro
 
 def main():
     all_days = True
     if len(sys.argv) >= 2 and sys.argv[1] == "today":
         all_days = False
 
-    home = os.path.expanduser("~")
-    worklogfn = os.path.join(home, PYMODORO_DIR, "worklog")
-
-    by_year = Counter()
-    by_month = Counter()
-    by_day = Counter()
-
-    prefix = "PYMODORO at "
-    with open(worklogfn) as infile:
-        for line in infile:
-            line = line.strip()
-            if line.startswith(prefix):
-                date_string = line[len(prefix):]
-                dt = datetime.datetime.strptime(date_string, DATETIMEFORMAT)
-                by_year[dt.year] += 1
-                by_month[(dt.year, dt.month)] += 1
-                by_day[(dt.year, dt.month, dt.day)] += 1
+    by_day = pymodoro.load_counts()
     for key in sorted(by_day.keys()):
         y, m, d = key
         if all_days:
